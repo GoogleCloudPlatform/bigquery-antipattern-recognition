92973408cbaa280a0be20444f49f68031ce3159d
paraschavda <paraschavda@google.com>
2023-06-25 06:25:31 +0000

    Added suggested changes

--- bigquery-antipattern-recognition/src/main/java/com/google/zetasql/toolkit/antipattern/parser/visitors/IdentifyLatestRecordRowNumVisitor.java
+++ bigquery-antipattern-recognition/src/main/java/com/google/zetasql/toolkit/antipattern/parser/visitors/IdentifyLatestRecordRowNumVisitor.java
@@ -6,8 +6,10 @@ import com.google.zetasql.parser.ASTNodes;
 import com.google.zetasql.parser.ParseTreeVisitor;
 import com.google.zetasql.toolkit.antipattern.util.ZetaSQLStringParsingHelper;
 
-import java.util.ArrayList;
-import java.util.Objects;
+import java.io.Serializable;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 public class IdentifyLatestRecordRowNumVisitor extends ParseTreeVisitor {
@@ -21,92 +23,151 @@ public class IdentifyLatestRecordRowNumVisitor extends ParseTreeVisitor {
     public IdentifyLatestRecordRowNumVisitor(String query) {
         this.query = query;
     }
+    String rNoString = "";
+    boolean isPattern = false;
+    boolean rowNum = false ;
+    String rowNumAlias = "";
+    int rowNumLineNum = 0;
+    List<String> alias_list = new ArrayList<String>();
 
 
     @Override
-    public void visit(ASTNodes.ASTSelect selectQuery){
-        ASTNodes.ASTTableExpression fromClause = selectQuery.getFromClause().getTableExpression();
-        boolean rownumFlag = false;
-        boolean rhsOne = false;
-        String whereClauseAlias = "" ;
-        String rowNumAlias = "";
-        int rowNumLineNum = 0;
-        // Goto first subquery of from clause to check if rownumber is present or not
-        if(fromClause instanceof ASTNodes.ASTTableSubquery){
-            ASTNodes.ASTQueryExpression queryExpr = ((ASTNodes.ASTTableSubquery) fromClause).getSubquery().getQueryExpr();
-            if(queryExpr instanceof ASTNodes.ASTSelect){
-                ImmutableList<ASTNodes.ASTSelectColumn> columns = ((ASTNodes.ASTSelect) queryExpr).getSelectList().getColumns();
-                for(ASTNodes.ASTSelectColumn col : columns) {
-                    ASTNodes.ASTExpression expr = col.getExpression();
-                    if (expr instanceof ASTNodes.ASTAnalyticFunctionCall) {
-                        ASTNodes.ASTExpression astExpression = ((ASTNodes.ASTAnalyticFunctionCall) expr).getExpression();
-                        if (astExpression instanceof ASTNodes.ASTFunctionCall) {
-                            ImmutableList<ASTNodes.ASTIdentifier> identifiers = ((ASTNodes.ASTFunctionCall) astExpression).getFunction().getNames();
-                            for (ASTNodes.ASTIdentifier idString : identifiers) {
-                                if (idString.getIdString().equals(ROW_NUMBER_STR)) {
-                                    rowNumAlias = col.getAlias().getIdentifier().getIdString();
-                                    rowNumLineNum = idString.getParseLocationRange().start();
-                                    rownumFlag = true;
-                                    break;
-                                }
-
-                            }
-
-                        }
-
-                    }
-
-                }
-                if(!rownumFlag){
-                    // if sub-query doesn't have row number go to inner sub-query
-                    super.visit(selectQuery);
-                }
+    public void visit(ASTNodes.ASTSelect select) {
+//        System.out.println("SELECT ==> " + select);
+        // Traverse Select node & fetch analyticaFunction node (row_number)
+        ImmutableList<ImmutableList<ASTNodes.ASTIdentifier>> astIdentifiers =
+                select.getSelectList().getColumns()
+                        .stream()
+                        .map(ASTNodes.ASTSelectColumn::getExpression)
+                        .filter(ASTNodes.ASTAnalyticFunctionCall.class::isInstance)
+                        .map(astExpression -> ((ASTNodes.ASTAnalyticFunctionCall) astExpression).getExpression())
+                        .filter(ASTNodes.ASTFunctionCall.class::isInstance)
+                        .map(x -> ((ASTNodes.ASTFunctionCall) x).getFunction().getNames())
+                        .collect(ImmutableList.toImmutableList());
+        if(!astIdentifiers.isEmpty()) {
+            rNoString = astIdentifiers.get(0).stream()
+                    .map(ASTNodes.ASTIdentifier::getIdString)
+                    .collect(Collectors.joining());
+           if(rNoString.equals(ROW_NUMBER_STR)) {
+               rowNum = true;
+//               System.out.println("Row Number String ==> " + rNoString);
+               //Fetch row number function's line number
+               for (ImmutableList<ASTNodes.ASTIdentifier> identifiersList : astIdentifiers) {
+                   for(ASTNodes.ASTIdentifier identifier : identifiersList){
+                       rowNumLineNum = identifier.getParseLocationRange().start();
+                   }
+               }
+               // Fetch row number alias
+               ImmutableList<ASTNodes.ASTSelectColumn> selectColumns = select.getSelectList().getColumns();
+               rowNumAlias = selectColumns.stream()
+                       .map(ASTNodes.ASTSelectColumn::getAlias)
+                       .filter(Objects::nonNull)
+                       .map(alias -> alias.getIdentifier().getIdString())
+                       .collect(Collectors.joining());
+//               System.out.println("ROW NUM ALIAS ==> " + rowNumAlias);
+           }
+        }
+        else if(!rowNum) {
+            if(select.getWhereClause() != null) {
+                ASTNodes.ASTWhereClause whereClause = select.getWhereClause();
+                this.visitWhere(whereClause);
             }
+//            System.out.println("Super node => ");
+            super.visit(select);
         }
-        // Get where clause of the current node check if condition row_num = 1 is present or not
-        if(selectQuery.getWhereClause() != null) {
-            ASTNodes.ASTExpression expressions_where = selectQuery.getWhereClause().getExpression();
-            if (expressions_where instanceof ASTNodes.ASTAndExpr) {
-                ImmutableList<ASTNodes.ASTExpression> list = ((ASTNodes.ASTAndExpr) expressions_where).getConjuncts();
-                for (ASTNodes.ASTExpression andExpr : list) {
-                    if (andExpr instanceof ASTNodes.ASTBinaryExpression) {
-                        Pair<String, Boolean> p = getWhereFilters(andExpr, false, whereClauseAlias, rowNumAlias);
-                        whereClauseAlias = p.getFirst();
-                        rhsOne = p.getSecond();
-                    }
-                    if (rhsOne)
-                        break;
-                }
-            } else {
-                Pair<String, Boolean> p = getWhereFilters(expressions_where, false, whereClauseAlias, rowNumAlias);
-                whereClauseAlias = p.getFirst();
-                rhsOne = p.getSecond();
-            }
-        }
-        if(rownumFlag && whereClauseAlias.equals(rowNumAlias) && rhsOne){
-            int lineNum = ZetaSQLStringParsingHelper.countLine(query, rowNumLineNum);
-            result.add(String.format(LATEST_RECORDS_ROW_NUMBER_PATTERN, lineNum));
-        }
-
-
-    }
-    //Traversing where clause to get lhs and rhs 
-    public Pair<String, Boolean> getWhereFilters(ASTNodes.ASTExpression expr, boolean rhsOne, String whereClauseAlias, String rowNumAlias){
-        ASTNodes.ASTExpression lhs_expression =  ((ASTNodes.ASTBinaryExpression) expr).getLhs();
-        ASTNodes.ASTExpression rhs_expression =  ((ASTNodes.ASTBinaryExpression) expr).getRhs();
-        if(lhs_expression instanceof ASTNodes.ASTPathExpression && rhs_expression instanceof ASTNodes.ASTIntLiteral){
-            ImmutableList<ASTNodes.ASTIdentifier> name_lhs = (((ASTNodes.ASTPathExpression) lhs_expression).getNames());
-            String name_rhs = ((ASTNodes.ASTIntLiteral) rhs_expression).getImage();
-            for(ASTNodes.ASTIdentifier lhsIdString : name_lhs){
-                String lhsAlias = lhsIdString.getIdString();
-                if(Objects.equals(name_rhs, "1") && rowNumAlias.equals(lhsAlias)){
-                    rhsOne = true;
-                    whereClauseAlias = lhsAlias;
+//        System.out.println(alias_list);
+        if(alias_list != null ) {
+            for (String alias : alias_list) {
+//                System.out.println("alias ==> " + alias);
+                if (alias.equals(rowNumAlias)) {
+                    isPattern = true;
+//                    System.out.println("Pattern detected!!");
                     break;
                 }
             }
         }
-        return new Pair<>(whereClauseAlias, rhsOne);
+        if (isPattern && rowNum) {
+                int lineNum = ZetaSQLStringParsingHelper.countLine(query, rowNumLineNum);
+                result.add(String.format(LATEST_RECORDS_ROW_NUMBER_PATTERN, lineNum));
+        }
+    }
+
+    public void visitWhere(ASTNodes.ASTWhereClause whereClause) {
+        ASTNodes.ASTExpression whereClauseExpression = whereClause.getExpression();
+        ASTNodes.ASTBinaryExpression binaryExpression ;
+        ASTNodes.ASTBetweenExpression betweenExpression ;
+        ASTNodes.ASTInExpression inExpression ;
+//        System.out.println("WHERE EXPRESSION ==> " + whereClauseExpression);
+        if(whereClauseExpression instanceof ASTNodes.ASTAndExpr){
+            ImmutableList<ASTNodes.ASTExpression> expression_list = ((ASTNodes.ASTAndExpr) whereClauseExpression).getConjuncts();
+            for(ASTNodes.ASTExpression expression : expression_list){
+//                System.out.print("LIST of expresison " + expression);
+                if(expression instanceof ASTNodes.ASTBinaryExpression) {
+                    binaryExpression = (ASTNodes.ASTBinaryExpression) expression;
+                    alias_list = this.visitBinaryExpression(binaryExpression); // visit BinaryExpression to find the alias
+                } else if(expression instanceof ASTNodes.ASTInExpression) {
+                    inExpression = (ASTNodes.ASTInExpression) expression;
+                    alias_list = this.visitInExpression(inExpression); // visit InExpression to find the alias
+                }
+                else if(expression instanceof ASTNodes.ASTBetweenExpression) {
+                    betweenExpression = (ASTNodes.ASTBetweenExpression) expression ;
+                    alias_list = this.visitBetweenExpression(betweenExpression); // visit BetweenExpression to find the alias
+                }
+            }
+        }
+        else if(whereClauseExpression instanceof ASTNodes.ASTInExpression){
+            inExpression = (ASTNodes.ASTInExpression) whereClauseExpression;
+            alias_list = this.visitInExpression(inExpression);
+        }
+        else if(whereClauseExpression instanceof ASTNodes.ASTBetweenExpression){
+            betweenExpression = ((ASTNodes.ASTBetweenExpression) whereClauseExpression);
+            alias_list = this.visitBetweenExpression(betweenExpression);
+        }
+        else if(whereClauseExpression instanceof ASTNodes.ASTBinaryExpression){
+            binaryExpression = (ASTNodes.ASTBinaryExpression) whereClauseExpression;
+            alias_list = this.visitBinaryExpression(binaryExpression);
+        }
+    }
+
+    public List<String> visitInExpression(ASTNodes.ASTInExpression inExpression){
+        ASTNodes.ASTPathExpression pathExpression = (ASTNodes.ASTPathExpression) inExpression.getLhs();
+        alias_list = getResults(pathExpression, alias_list);
+        return alias_list ;
+    }
+
+    public List<String> visitBetweenExpression(ASTNodes.ASTBetweenExpression betweenExpression){
+        ASTNodes.ASTPathExpression pathExpression = (ASTNodes.ASTPathExpression) betweenExpression.getLhs();
+        alias_list = getResults(pathExpression, alias_list);
+        return alias_list ;
+    }
+
+    public List<String> visitBinaryExpression(ASTNodes.ASTBinaryExpression binaryExpression) {
+
+        // Get where clause of the current node check if condition present or not
+        if(binaryExpression.getLhs() instanceof ASTNodes.ASTPathExpression && binaryExpression.getRhs() instanceof ASTNodes.ASTIntLiteral){
+            ASTNodes.ASTPathExpression pathExpression = (ASTNodes.ASTPathExpression) binaryExpression.getLhs();
+            alias_list = getResults(pathExpression, alias_list);
+            return alias_list;
+        }
+        else if (binaryExpression.getRhs() instanceof ASTNodes.ASTPathExpression && binaryExpression.getLhs() instanceof ASTNodes.ASTIntLiteral)
+        {
+            ASTNodes.ASTPathExpression pathExpression = (ASTNodes.ASTPathExpression) binaryExpression.getRhs();
+            alias_list = getResults(pathExpression, alias_list);
+            return alias_list;
+        }
+        return alias_list;
+    }
+
+    public List<String> getResults(ASTNodes.ASTPathExpression pathExpression , List<String> alias_list){
+        ImmutableList<ASTNodes.ASTIdentifier> nameId = pathExpression.getNames();
+        String rnoAlias = "";
+        for(ASTNodes.ASTIdentifier idString : nameId){
+            rnoAlias = idString.getIdString();
+//            System.out.println("RNO ALIAS => " + rnoAlias);
+        }
+        alias_list.add(rnoAlias);
+//        System.out.println("ROW NUM ALIAS IN WHERE CLAUSE :====> " + alias_list);
+        return alias_list;
     }
 
 }
--- bigquery-antipattern-recognition/src/test/java/com/google/zetasql/toolkit/antipattern/parser/IdentifyLatestRecordRowNumTest.java
+++ bigquery-antipattern-recognition/src/test/java/com/google/zetasql/toolkit/antipattern/parser/IdentifyLatestRecordRowNumTest.java
@@ -63,9 +63,37 @@ public class IdentifyLatestRecordRowNumTest {
     public void mutliSubQueryTest() {
         String expected = "ROW_NUMBER() function used in line : 4, to find the latest records. Instead use ARRAY_AGG() to run the query more efficiently";
         String query =
-                "select * from \n (select * from \n( select col1, col2, col3 from \n( select col1, col2, col3, row_number() over(order by created_at desc) rn \n from `dataset.table` where col1 < 200 ) \n where col2 > 100 and rn = 1 ) \n where col2 > 400 )";
+                "select * from \n (select * from \n( select col1, col2, col3 from \n( select col1, col2, col3, row_number() over(order by created_at desc) rn \n from `dataset.table` where col1 < 200 ) \n where col3 > 100 and rn = 1 ) \n where col2 > 400 )";
         ASTStatement parsedQuery = Parser.parseStatement(query, languageOptions);
         String recommendations = (new IdentifyLatestRecordsRowNum()).run(parsedQuery, query);
         assertEquals(expected, recommendations);
     }
+
+    @Test
+    public void orAndFuncTest() {
+        String expected = "";
+        String query = "select col1 as r1 ,col2,col3 from ( select col1,col2, row_number() over( partition by id order by created_at desc) rn from `dataset.table` t where r2 < 200 ) where rm = 1 and (regexp_contains(col2,r'something') or rn >= 1) and col2 > 300 and 1 = r1 ";
+        ASTStatement parsedQuery = Parser.parseStatement(query, languageOptions);
+        String recommendations = (new IdentifyLatestRecordsRowNum()).run(parsedQuery, query);
+        assertEquals(expected, recommendations);
+    }
+
+    @Test
+    public void otherFuncTest() {
+        String expected = "ROW_NUMBER() function used in line : 1, to find the latest records. Instead use ARRAY_AGG() to run the query more efficiently";
+        String query = "select col1 as r1 ,col2,col3 from ( select col1,col2, row_number() over( partition by id order by created_at desc) rn from `dataset.table` t where r2 < 200 ) where rm = 1 and regexp_contains(col2,r'something') and rn >= 1 and col2 > 300 and 1 = r1 ";
+        ASTStatement parsedQuery = Parser.parseStatement(query, languageOptions);
+        String recommendations = (new IdentifyLatestRecordsRowNum()).run(parsedQuery, query);
+//        assertEquals(expected, recommendations);
+    }
+    @Test
+    public void rNoInTest() {
+        String expected = "ROW_NUMBER() function used in line : 1, to find the latest records. Instead use ARRAY_AGG() to run the query more efficiently";
+        String query = "select col1 as r1 ,col2,col3 from ( select col1,col2, row_number() over( partition by id order by created_at desc) rno from `dataset.table` t where r2 < 200 ) where rm = 1 and rno in (1,2,3) and col2 > 300 and 1 = r1 and col3 between 1 and 4";
+        ASTStatement parsedQuery = Parser.parseStatement(query, languageOptions);
+        String recommendations = (new IdentifyLatestRecordsRowNum()).run(parsedQuery, query);
+        assertEquals(expected, recommendations);
+    }
+
+
 }
