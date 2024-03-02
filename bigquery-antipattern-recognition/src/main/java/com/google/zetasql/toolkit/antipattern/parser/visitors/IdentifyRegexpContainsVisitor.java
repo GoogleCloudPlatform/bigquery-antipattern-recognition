2059ed13b38c81b52a6150b38a3eb19330e99e1d
Franklin Whaite <whaite@google.com>
2024-03-02 14:42:36 +0000

    enhancing precision of string comparison anti pattern

--- bigquery-antipattern-recognition/src/main/java/com/google/zetasql/toolkit/antipattern/parser/visitors/IdentifyRegexpContainsVisitor.java
+++ bigquery-antipattern-recognition/src/main/java/com/google/zetasql/toolkit/antipattern/parser/visitors/IdentifyRegexpContainsVisitor.java
@@ -8,12 +8,15 @@ import com.google.zetasql.toolkit.antipattern.AntiPatternVisitor;
 import com.google.zetasql.toolkit.antipattern.util.ZetaSQLStringParsingHelper;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class IdentifyRegexpContainsVisitor extends ParseTreeVisitor implements AntiPatternVisitor {
 
     public final static String NAME = "StringComparison";
-    private final static String REGEXP_CONTAINS = "REGEXP_CONTAINS at line %d. Prefer LIKE when the full power of regex is not needed (e.g. wildcard matching).";
-    private final static String REGEXP_CONTAINS_STR = "regexp_contains";
+    private final static String REGEXP_CONTAINS_ANTI_PATTERN_MESSAGE = "REGEXP_CONTAINS at line %d. Prefer LIKE when the full power of regex is not needed (e.g. wildcard matching).";
+    private final static String REGEXP_CONTAINS_FUN_ID_STR = "regexp_contains";
+    private final static String REGEX_STRING = "['\"]\\.\\*.*\\.\\*['\"]";
     private ArrayList<String> result = new ArrayList<String>();
     private String query;
 
@@ -28,11 +31,22 @@ public class IdentifyRegexpContainsVisitor extends ParseTreeVisitor implements A
     @Override
     public void visit(ASTNodes.ASTFunctionCall node) {
         ImmutableList<ASTNodes.ASTIdentifier> identifiers = node.getFunction().getNames();
-
         for (ASTNodes.ASTIdentifier identifier : identifiers) {
-            if(identifier.getIdString().equals(REGEXP_CONTAINS_STR)){
-                int lineNum = ZetaSQLStringParsingHelper.countLine(query, identifier.getParseLocationRange().start());
-                result.add(String.format(REGEXP_CONTAINS, lineNum));
+            // search for regexp_contains
+            if(identifier.getIdString().toLowerCase().equals(REGEXP_CONTAINS_FUN_ID_STR)){
+                ImmutableList<ASTNodes.ASTExpression> arguments = node.getArguments();
+                // search for argument with string
+                for(ASTNodes.ASTExpression argument: arguments) {
+                    if(argument instanceof ASTNodes.ASTStringLiteral){
+                        String stringLiteralArg = ((ASTNodes.ASTStringLiteral) argument).getImage();
+                        Pattern pattern = Pattern.compile(REGEX_STRING);
+                        Matcher matcher = pattern.matcher(stringLiteralArg);
+                        if(matcher.find()) {
+                            int lineNum = ZetaSQLStringParsingHelper.countLine(query, identifier.getParseLocationRange().start());
+                            result.add(String.format(REGEXP_CONTAINS_ANTI_PATTERN_MESSAGE, lineNum));
+                        }
+                    }
+                }
             }
         }
 
