4971ecec27d499d25f244bafe59acd0a7afbe999
Franklin Whaite <whaite@google.com>
2024-03-17 10:50:48 -0500

    adding documentation on running with airflow

---
+++ airflow/antipattern_recognition_dag.py
@@ -0,0 +1,31 @@
+import datetime
+
+from airflow import models
+from airflow.operators import bash
+
+
+YESTERDAY = datetime.datetime.now() - datetime.timedelta(days=1)
+
+default_args = {
+    "owner": "Composer Antipattern Example",
+    "depends_on_past": False,
+    "email": [""],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": datetime.timedelta(minutes=5),
+    "start_date": YESTERDAY,
+}
+
+with models.DAG(
+    "antipattern_recognition_dag",
+    catchup=False,
+    default_args=default_args,
+    schedule_interval=datetime.timedelta(days=1),
+) as dag:
+    # Print the dag_run id from the Airflow logs
+    print_dag_run_conf = bash.BashOperator(
+        task_id="run_antipattern_recognition_tool", bash_command='java -jar /home/airflow/gcs/plugins/bigquery-antipattern-recognition-0.1.1-SNAPSHOT-jar-with-dependencies.jar --query "select * from table1"'
+    )
+
+
