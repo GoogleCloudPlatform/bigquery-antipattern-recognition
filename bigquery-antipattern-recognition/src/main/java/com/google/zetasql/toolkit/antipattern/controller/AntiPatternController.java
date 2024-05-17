61b187028fd973b0f2b786e6389079af6ac4263c
Andrew Fleischer <afleisc@google.com>
2024-05-17 17:13:48 -0500

    mvn spotless

--- bigquery-antipattern-recognition/src/main/java/com/google/zetasql/toolkit/antipattern/controller/AntiPatternController.java
+++ bigquery-antipattern-recognition/src/main/java/com/google/zetasql/toolkit/antipattern/controller/AntiPatternController.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package com.google.zetasql.toolkit.antipattern.controller;
 
 import com.google.zetasql.toolkit.antipattern.Main;
@@ -11,15 +27,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class AntiPatternController {
 
     @PostMapping("/")
-    public String analyzeQuery(@RequestBody String query) { // Change to @RequestParam
+    public String analyzeQuery(@RequestBody String query) {
         try {
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
             System.setOut(new PrintStream(baos));
-
-            // Execute the Main class with the query as a command-line argument
             String[] args = {"--query", query};
             Main.main(args);
-
             System.setOut(System.out);
             return baos.toString();
         } catch (Exception e) {
