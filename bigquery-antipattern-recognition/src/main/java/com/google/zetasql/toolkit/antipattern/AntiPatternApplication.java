1dc149e308a1d4e5a4a1035c246a75613fe3e8d0
Andrew Fleischer <afleisc@google.com>
2024-05-17 17:02:31 -0500

    antipattern controller

--- bigquery-antipattern-recognition/src/main/java/com/google/zetasql/toolkit/antipattern/AntiPatternApplication.java
+++ bigquery-antipattern-recognition/src/main/java/com/google/zetasql/toolkit/antipattern/AntiPatternApplication.java
@@ -1,4 +1,4 @@
-package com.google.zetasql.toolkit.antipattern; // Use your actual package name
+package com.google.zetasql.toolkit.antipattern; 
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AntiPatternApplication {
 
     public static void main(String[] args) {
-        // This is the standard way to start a Spring Boot application
         SpringApplication.run(AntiPatternApplication.class, args); 
     }
 }
--- bigquery-antipattern-recognition/src/main/java/com/google/zetasql/toolkit/antipattern/controller/AntiPatternController.java
+++ bigquery-antipattern-recognition/src/main/java/com/google/zetasql/toolkit/antipattern/controller/AntiPatternController.java
@@ -1,12 +1,29 @@
 package com.google.zetasql.toolkit.antipattern.controller;
+
+import com.google.zetasql.toolkit.antipattern.Main;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AntiPatternController {
 
     @PostMapping("/")
-    String hello() {
-        return "Hello !";
-      }
+    public String analyzeQuery(@RequestBody String query) { // Change to @RequestParam
+        try {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(baos));
+
+            // Execute the Main class with the query as a command-line argument
+            String[] args = {"--query", query};
+            Main.main(args);
+
+            System.setOut(System.out);
+            return baos.toString();
+        } catch (Exception e) {
+            return "Error processing the query: " + e.getMessage();
+        }
+    }
 }
