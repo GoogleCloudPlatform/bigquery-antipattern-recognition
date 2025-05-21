d26e5d30397cf94453643bb173767bffbe374653
Tunahan <49438832+TunahanOcal@users.noreply.github.com>
2025-05-21 13:53:24 +0300

    spotless fix

--- bigquery-antipattern-recognition/src/main/java/com/google/zetasql/toolkit/antipattern/util/AntiPatternHelper.java
+++ bigquery-antipattern-recognition/src/main/java/com/google/zetasql/toolkit/antipattern/util/AntiPatternHelper.java
@@ -1,21 +1,22 @@
 /*
-* Copyright (C) 2024 Google LLC
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not
-* use this file except in compliance with the License. You may obtain a copy of
-* the License at
-*
-*   http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-* License for the specific language governing permissions and limitations under
-* the License.
-*/
+ * Copyright (C) 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 
 package com.google.zetasql.toolkit.antipattern.util;
+
 import com.google.zetasql.AnalyzerOptions;
 import com.google.zetasql.LanguageOptions;
 import com.google.zetasql.Parser;
@@ -172,4 +173,3 @@ public class AntiPatternHelper {
         return new ZetaSQLToolkitAnalyzer(options);
     }
 }
- 
